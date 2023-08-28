309c838407d01c4469e6fdf4bc97e4205dc68320
AndreiMorariu <andreimorariu4140@gmail.com>
2023-08-28 17:08:16 +0300

    m

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -4,6 +4,7 @@ function Cv() {
       <a
         href='./CV.pdf'
         className='inline-block text-2xl border-b-2 border-emerald-300 pb-2 mt-10 hover:text-slate-200'
+        target='_blank'
         download
       >
         Download my CV
