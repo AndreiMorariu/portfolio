d97a21821cddea1d7ec01fab32f255dd9e2d18a5
AndreiMorariu <andreimorariu4140@gmail.com>
2024-01-31 12:43:42 +0200

    Fixed hidden text

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,22 +4,21 @@ function Project({ project }) {
       <img
         src={`./${project.image}`}
         alt={project.name}
-        // className='rounded-lg lg:w-4/5 aspect-video object-cover object-top mx-auto'
         className='rounded-lg w-full h-full aspect-video object-cover object-top'
       />
       <div className='flex flex-col sm:max-lg:justify-between h-full lg:gap-6'>
         <header>
-          <h3 className='font-bold uppercase text-xl lg:text-2xl mt-2 md:mt-0 lg:mt-0 md:mb-2'>
+          <h3 className='font-bold uppercase text-xl lg:text-2xl mb-4 mt-2 md:mt-0 md:mb-2 lg:mt-0 '>
             {project.name}
           </h3>
-          <div className='flex gap-6 text-emerald-200 md:text-lg lg:text-xl md:mb-2'>
+          <div className='flex gap-6 text-emerald-200 md:text-lg lg:text-xl mb-4 first-line:md:mb-2'>
             {project.technologies.map((item, i) => (
               <span key={i}>{item}</span>
             ))}
           </div>
         </header>
         <div className='flex flex-col h-full'>
-          <p className='hidden md:block text-slate-200 text-md lg:text-lg'>
+          <p className='text-slate-200 text-md lg:text-lg'>
             {project.description}
           </p>
           <div className='flex gap-6 uppercase mt-3 md:self-end lg:mt-auto lg:pt-4'>
