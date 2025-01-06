56b1f5aca15d24c087f4ae19c925f7b8dfcc6833
AndreiMorariu <109082840+AndreiMorariu@users.noreply.github.com>
2025-01-06 10:55:19 +0200

    Added project type

---
+++ src/types/project.ts
@@ -0,0 +1,12 @@
+export interface Project {
+	name: string;
+	image: string;
+	technologies: string[];
+	description: string;
+	longDescription: string;
+	purpose: string;
+	features: string[];
+	technologiesDetailed: string[];
+	linkCode: string;
+	linkProject?: string;
+}
